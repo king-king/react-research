b6ff38afcbab2791d92353eb2e7facd3f2ea2706
wangqun6 <wangqun28>
2017-05-15 17:41:43 +0800

    webpack

---
+++ test/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by wangqun6 on 2017/5/15.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+<h1>
+<div>Hello, world!</div>
+</h1>
+    ,
+    document.querySelector('.example')
+);
