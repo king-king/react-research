7236fd710169b34b586475774a3e0b056241a43c
king-king <niuyiwangqun@sohu.com>
2018-12-04 01:14:32 +0800

    change

--- packages/react/src/React.js
+++ packages/react/src/React.js
@@ -7,65 +7,65 @@
 
 import ReactVersion from 'shared/ReactVersion';
 import {
-  REACT_CONCURRENT_MODE_TYPE,
-  REACT_FRAGMENT_TYPE,
-  REACT_PROFILER_TYPE,
-  REACT_STRICT_MODE_TYPE,
-  REACT_SUSPENSE_TYPE,
+    REACT_CONCURRENT_MODE_TYPE,
+    REACT_FRAGMENT_TYPE,
+    REACT_PROFILER_TYPE,
+    REACT_STRICT_MODE_TYPE,
+    REACT_SUSPENSE_TYPE,
 } from 'shared/ReactSymbols';
 
-import {Component, PureComponent} from './ReactBaseClasses';
-import {createRef} from './ReactCreateRef';
-import {forEach, map, count, toArray, only} from './ReactChildren';
+import { Component, PureComponent } from './ReactBaseClasses';
+import { createRef } from './ReactCreateRef';
+import { forEach, map, count, toArray, only } from './ReactChildren';
 import {
-  createElement,
-  createFactory,
-  cloneElement,
-  isValidElement,
+    createElement,
+    createFactory,
+    cloneElement,
+    isValidElement,
 } from './ReactElement';
-import {createContext} from './ReactContext';
-import {lazy} from './ReactLazy';
+import { createContext } from './ReactContext';
+import { lazy } from './ReactLazy';
 import forwardRef from './forwardRef';
 import pure from './pure';
 import {
-  createElementWithValidation,
-  createFactoryWithValidation,
-  cloneElementWithValidation,
+    createElementWithValidation,
+    createFactoryWithValidation,
+    cloneElementWithValidation,
 } from './ReactElementValidator';
 import ReactSharedInternals from './ReactSharedInternals';
 
 const React = {
-  Children: {
-    map,
-    forEach,
-    count,
-    toArray,
-    only,
-  },
+    Children: {
+        map,
+        forEach,
+        count,
+        toArray,
+        only,
+    },
 
-  createRef,
-  Component,
-  PureComponent,
+    createRef,
+    Component,
+    PureComponent,
 
-  createContext,
-  forwardRef,
-  lazy,
-  pure,
+    createContext,
+    forwardRef,
+    lazy,
+    pure,
 
-  Fragment: REACT_FRAGMENT_TYPE,
-  StrictMode: REACT_STRICT_MODE_TYPE,
-  unstable_ConcurrentMode: REACT_CONCURRENT_MODE_TYPE,
-  unstable_Suspense: REACT_SUSPENSE_TYPE,
-  unstable_Profiler: REACT_PROFILER_TYPE,
+    Fragment: REACT_FRAGMENT_TYPE,
+    StrictMode: REACT_STRICT_MODE_TYPE,
+    unstable_ConcurrentMode: REACT_CONCURRENT_MODE_TYPE,
+    unstable_Suspense: REACT_SUSPENSE_TYPE,
+    unstable_Profiler: REACT_PROFILER_TYPE,
 
-  createElement: __DEV__ ? createElementWithValidation : createElement,
-  cloneElement: __DEV__ ? cloneElementWithValidation : cloneElement,
-  createFactory: __DEV__ ? createFactoryWithValidation : createFactory,
-  isValidElement: isValidElement,
+    createElement: __DEV__ ? createElementWithValidation : createElement,
+    cloneElement: __DEV__ ? cloneElementWithValidation : cloneElement,
+    createFactory: __DEV__ ? createFactoryWithValidation : createFactory,
+    isValidElement: isValidElement,
 
-  version: ReactVersion,
+    version: ReactVersion,
 
-  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: ReactSharedInternals,
+    __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: ReactSharedInternals,
 };
 
 export default React;
--- packages/shared/ReactSymbols.js
+++ packages/shared/ReactSymbols.js
@@ -12,49 +12,49 @@
 const hasSymbol = typeof Symbol === 'function' && Symbol.for;
 
 export const REACT_ELEMENT_TYPE = hasSymbol
-  ? Symbol.for('react.element')
-  : 0xeac7;
+    ? Symbol.for('react.element')
+    : 0xeac7;
 export const REACT_PORTAL_TYPE = hasSymbol
-  ? Symbol.for('react.portal')
-  : 0xeaca;
+    ? Symbol.for('react.portal')
+    : 0xeaca;
 export const REACT_FRAGMENT_TYPE = hasSymbol
-  ? Symbol.for('react.fragment')
-  : 0xeacb;
+    ? Symbol.for('react.fragment')
+    : 0xeacb;
 export const REACT_STRICT_MODE_TYPE = hasSymbol
-  ? Symbol.for('react.strict_mode')
-  : 0xeacc;
+    ? Symbol.for('react.strict_mode')
+    : 0xeacc;
 export const REACT_PROFILER_TYPE = hasSymbol
-  ? Symbol.for('react.profiler')
-  : 0xead2;
+    ? Symbol.for('react.profiler')
+    : 0xead2;
 export const REACT_PROVIDER_TYPE = hasSymbol
-  ? Symbol.for('react.provider')
-  : 0xeacd;
+    ? Symbol.for('react.provider')
+    : 0xeacd;
 export const REACT_CONTEXT_TYPE = hasSymbol
-  ? Symbol.for('react.context')
-  : 0xeace;
+    ? Symbol.for('react.context')
+    : 0xeace;
 export const REACT_CONCURRENT_MODE_TYPE = hasSymbol
-  ? Symbol.for('react.concurrent_mode')
-  : 0xeacf;
+    ? Symbol.for('react.concurrent_mode')
+    : 0xeacf;
 export const REACT_FORWARD_REF_TYPE = hasSymbol
-  ? Symbol.for('react.forward_ref')
-  : 0xead0;
+    ? Symbol.for('react.forward_ref')
+    : 0xead0;
 export const REACT_SUSPENSE_TYPE = hasSymbol
-  ? Symbol.for('react.suspense')
-  : 0xead1;
+    ? Symbol.for('react.suspense')
+    : 0xead1;
 export const REACT_PURE_TYPE = hasSymbol ? Symbol.for('react.pure') : 0xead3;
 
 const MAYBE_ITERATOR_SYMBOL = typeof Symbol === 'function' && Symbol.iterator;
 const FAUX_ITERATOR_SYMBOL = '@@iterator';
 
-export function getIteratorFn(maybeIterable: ?any): ?() => ?Iterator<*> {
-  if (maybeIterable === null || typeof maybeIterable !== 'object') {
+export function getIteratorFn(maybeIterable: ?any): ?() =>?Iterator<*> {
+    if (maybeIterable === null || typeof maybeIterable !== 'object') {
+        return null;
+    }
+    const maybeIterator =
+        (MAYBE_ITERATOR_SYMBOL && maybeIterable[MAYBE_ITERATOR_SYMBOL]) ||
+        maybeIterable[FAUX_ITERATOR_SYMBOL];
+    if (typeof maybeIterator === 'function') {
+        return maybeIterator;
+    }
     return null;
-  }
-  const maybeIterator =
-    (MAYBE_ITERATOR_SYMBOL && maybeIterable[MAYBE_ITERATOR_SYMBOL]) ||
-    maybeIterable[FAUX_ITERATOR_SYMBOL];
-  if (typeof maybeIterator === 'function') {
-    return maybeIterator;
-  }
-  return null;
 }
